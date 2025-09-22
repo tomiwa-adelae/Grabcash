8bc004321293245df953342c26a81bf01d067497
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2025-09-22 22:06:05 +0100

    Fixed an error

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
-export const Logo = ({ size }: { size: string }) => {
+export const Logo = ({ size }: { size?: string }) => {
   return (
     <Link className={cn("font-semibold text-3xl text-primary", size)} href="/">
       earnsphere
