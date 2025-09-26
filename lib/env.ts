a9c78d92b77f37068ec6709ced2677d228ef16a5
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2025-09-26 10:35:49 +0100

    Worked on the site renaming

--- lib/env.ts
+++ lib/env.ts
@@ -9,7 +9,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
-    EARNSPHERE_SENDING_EMAIL: z.string().min(1),
+    GRABCASH_SENDING_EMAIL: z.string().min(1),
     AWS_ACCESS_KEY_ID: z.string().min(1),
     AWS_SECRET_ACCESS_KEY: z.string().min(1),
     AWS_ENDPOINT_URL_S3: z.string().min(1),
