4590d7bacd31bf82c9abd68aa3572d4b308b7746
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2025-09-27 16:48:19 +0100

    Worked on the UX

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode } from "react";
 import { Header } from "./_components/Header";
+import { Header as MemberHeader } from "../(member)/_components/Header";
 import { Footer } from "./_components/Footer";
 import { PageGradient } from "@/components/PageGradient";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <div>
-      <Header />
+      {session === null ? <Header /> : <MemberHeader />}
       <div className="pt-20 relative">
         <PageGradient />
         {children}
